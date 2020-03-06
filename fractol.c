8191d3aea44b87fd6d63226a49363f94c7c089bf
Magan Josue <majosue@at-k1.21-school.ru>
2020-03-06 14:23:02 +0300

    center_zoom

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 12:20:29 by majosue           #+#    #+#             */
-/*   Updated: 2020/01/21 13:52:51 by majosue          ###   ########.fr       */
+/*   Updated: 2020/03/06 14:20:15 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,10 @@ int ft_win_to_log(t_mlx *mlx, int i)
   
 	x = i % WIN_WIDTH;
 	y = i / WIN_WIDTH;
-	mlx->pnt[i].re = (x / ((1.0 / 4) * WIN_WIDTH) - 3); 
-	mlx->pnt[i].im = (y / ((-1.0 / 3) *  WIN_HEIGHT) + 1.5);
+	mlx->pnt[i].re = x * (4.0 * mlx->scale) / WIN_WIDTH;/// - ((4.0 * mlx->scale) / WIN_WIDTH) * WIN_WIDTH / 2; 
+	mlx->pnt[i].im = y * (4.0 * mlx->scale) / WIN_HEIGHT;// - ((4.0 * mlx->scale)/ WIN_WIDTH) * WIN_WIDTH / 2;
+//		printf("re = %f, im = %f\n", mlx->pnt[i].re, mlx->pnt[i].re);
+	mlx->pnt[i].color = 0;
 return (1);
 }
 
@@ -47,6 +49,9 @@ void ft_array_init(t_mlx *mlx)
 	{
 		ft_win_to_log(mlx, i);
 	}
+	mlx->pnt[n].re = 0;
+	mlx->pnt[n].im = 0;
+	mlx->n = n;
 }
 void ft_mand(double re, double im, t_mlx *mlx, int i)
 {
@@ -55,28 +60,19 @@ void ft_mand(double re, double im, t_mlx *mlx, int i)
 }
 int ft_get_colour(t_mlx *mlx, int iter)
 {
-	mlx->col.c[0] = iter * 5;
-	mlx->col.c[1] = iter * 7;
-	mlx->col.c[2] = iter * 2;
-	return (mlx->col.colour);
-}
+	t_colour c;
+	c.c[0] = iter * 10;
+	c.c[1] = iter * 10;
+	c.c[2] = iter * 10;
+	return (c.colour);
+} 
 
-int ft_zoom(int key, t_mlx *mlx)
+/* int		ft_mouse_move(int x, int y, t_mlx *mlx)
 {
-	mlx->scale = (key == 5 || key == 78) ? mlx->scale * 1.2 : mlx->scale / 1.2;
-	//mlx->scale = (key == 5 || key == 78) ? 1.1 : 0.9;
-	return (1); 
-}
-int		ft_mouse_move(int x, int y, t_mlx *mlx)
-{
-	/* mlx->mouse_x = ((x / ((1.0 / 4) * WIN_WIDTH) - 3) ) * mlx->scale + mlx->center_x;
-	mlx->mouse_y = ((y / ((-1.0 / 3) *  WIN_HEIGHT) + 1.5) ) * mlx->scale + mlx->center_y; */
-	mlx->mouse_x  = (x / ((1.0 / 4) * WIN_WIDTH) - 3) * mlx->scale;
-	mlx->mouse_y  = (y / ((-1.0 / 3) *  WIN_HEIGHT) + 1.5) * mlx->scale;
 	printf("zoom = %f, mouse recalc = %f, %f\n",mlx->scale, mlx->mouse_x, mlx->mouse_y);
 	return (0);
-}
-void ft_draw_cord(t_mlx *mlx)
+} */
+/* void ft_draw_cord(t_mlx *mlx)
 {
 	int i;
 	int n;
@@ -86,22 +82,24 @@ void ft_draw_cord(t_mlx *mlx)
 
 	while (++i < n)
 	{
-		if (!(mlx->pnt[i].re - mlx->mouse_x) || !(mlx->pnt[i].im - mlx->mouse_y))
+		if (mlx->pnt[i].re == mlx->center_x || mlx->pnt[i].im == mlx->center_y)
 		{
 			mlx->img.data[i] = 0xFFFFFF;
 		}
 	}
 
-}
-int ft_draw(t_mlx *mlx)
+} */
+/* int ft_draw(t_mlx *mlx)
 {
 	int m;
 	int i;
 	int iteration;
 	double dx;
 	double dy;
+	
 	t_point c;
-	ft_array_init(mlx);		
+	
+	//ft_array_init(mlx);		
 	iteration = 0;
 	mlx->col.colour = 0;
 	i = -1;
@@ -112,18 +110,14 @@ int ft_draw(t_mlx *mlx)
 	i = -1;
 	while (++i < m)
 	{
-		
-		mlx->pnt[i].re *= mlx->scale; // переставили в ноль и смасштабировали
+		mlx->pnt[i].re *= mlx->scale; // смасштабировали
 		mlx->pnt[i].im *= mlx->scale; //
 		dx = mlx->mouse_x - mlx->mouse_x * mlx->scale;
 		dy = mlx->mouse_y - mlx->mouse_y * mlx->scale;
 		mlx->pnt[i].re = mlx->pnt[i].re + dx;  // вернули обратно соответственно все координаты окна уже другие 
 		mlx->pnt[i].im = mlx->pnt[i].im + dy;  //
-	/*
-		когда берем следующи цент масштабирования нужно пересчитывать точку как если бы мы изначально тыкались в нее?
-	*/ 
-	//	printf("center object = %f, %f\n", mlx->center_x, mlx->center_y);
-	
+		
+
 		c = mlx->pnt[i];
 		while (pow(mlx->pnt[i].re, 2.0) + pow(mlx->pnt[i].im, 2.0) <= 4 && iteration < MAX_ITERATIONS)
         {
@@ -131,19 +125,62 @@ int ft_draw(t_mlx *mlx)
 			iteration++;
         }
 		mlx->pnt[i] = c;
-		//ft_log_to_win(mlx); // вот тут должны сместить относительно мышки, смасштабировать и вернуть обратно
 		if (iteration == MAX_ITERATIONS)	
 			mlx->img.data[i] = 0;
 		else 
 			mlx->img.data[i] = ft_get_colour(mlx, iteration);
 		iteration = 0;
 	}
-	//ft_log_to_win(mlx);
-	ft_draw_cord(mlx);
 	mlx_put_image_to_window((*mlx).mlx_ptr, (*mlx).win, (*mlx).img.img_ptr, 0, 0);
 	return (1);
 }
+ */
 
+int ft_calculate_pts(t_mlx *mlx)
+{
+{
+	int m;
+	int i;
+	int iteration;
+	
+	m = WIN_WIDTH * WIN_HEIGHT;
+	t_point c;
+	iteration = 0;
+	i = -1;
+	while (++i < m)
+	{
+		/* mlx->pnt[i].re *= mlx->scale; // смасштабировали
+		mlx->pnt[i].im *= mlx->scale; //
+		dx = mlx->mouse_x - mlx->mouse_x * mlx->scale;
+		dy = mlx->mouse_y - mlx->mouse_y * mlx->scale;
+		mlx->pnt[i].re = mlx->pnt[i].re + dx;  // вернули обратно соответственно все координаты окна уже другие 
+		mlx->pnt[i].im = mlx->pnt[i].im + dy;  //
+		 */
+
+		c = mlx->pnt[i]; //запомнили точку
+		while (pow(mlx->pnt[i].re, 2.0) + pow(mlx->pnt[i].im, 2.0) <= 4 && iteration < MAX_ITERATIONS)
+        {
+			ft_mand(pow(mlx->pnt[i].re, 2.0) - pow(mlx->pnt[i].im, 2.0) + c.re, 2.0 * mlx->pnt[i].re * mlx->pnt[i].im + c.im, mlx, i);
+			iteration++;
+        }
+		mlx->pnt[i] = c; //вернули взад
+		if (iteration == MAX_ITERATIONS)	
+			mlx->img.data[i] = 0;
+		else 
+			{
+				mlx->img.data[i] = ft_get_colour(mlx, iteration);
+				
+			}
+			//printf("color = %d\n", mlx->img.data[i]);
+		iteration = 0;
+	}
+	return (1);
+}
+
+}
+
+
+/* 
 int ft_keypress(int keycode, void *mlx)
 {
 	if(keycode == 53)
@@ -154,23 +191,89 @@ int ft_keypress(int keycode, void *mlx)
 			ft_draw(mlx);
 		}
 	return (1);
-}
-
-int		ft_mouse_press(int button, int x, int y, t_mlx *mlx) // вот тут надо как то компенсировать
+} */
+int ft_zoom(int key, t_mlx *mlx)
 {
+	int i;
+	double dx;
+	double dy;
 
 
-mlx->center_x = mlx->mouse_x * mlx->scale - mlx->mouse_x;
-mlx->center_y = mlx->mouse_y * mlx->scale - mlx->mouse_y;
-mlx->mouse_xp  = mlx->mouse_x;
-mlx->mouse_yp  = mlx->mouse_y;
-mlx->mouse_x  = (x / ((1.0 / 4) * WIN_WIDTH) - 3);
-mlx->mouse_y  = (y / ((-1.0 / 3) *  WIN_HEIGHT) + 1.5);
+	i = -1;
+	
+	mlx->scale = (key == 5 || key == 78) ? 1.1 : 0.9;
+	
+	
+	dx = mlx->mouse_x - mlx->mouse_x * mlx->scale;
+	dy = mlx->mouse_y - mlx->mouse_y * mlx->scale; 
+	
+	while (++i < mlx->n)
+	{
+	//mlx->pnt[i].re *= mlx->scale; //(mlx->pnt[i].re - mlx->mouse_x) * mlx->scale + mlx->mouse_x; 
+	//mlx->pnt[i].im *= mlx->scale; //(mlx->pnt[i].im - mlx->mouse_y) * mlx->scale + mlx->mouse_y; 
+	
+		mlx->pnt[i].re = mlx->pnt[i].re * mlx->scale + dx;  // вернули обратно соответственно все координаты окна уже другие 
+		mlx->pnt[i].im = mlx->pnt[i].im * mlx->scale + dy;  //
+		
 
+		//mlx->pnt[i].re = mlx->pnt[i].re + dx;  // вернули обратно соответственно все координаты окна уже другие 
+		//mlx->pnt[i].im = mlx->pnt[i].im + dy;  //		
+	}
+	
+	mlx->mouse_shift_x = mlx->mouse_shift_x * mlx->scale - dx;
+	mlx->mouse_shift_y = mlx->mouse_shift_y * mlx->scale - dy;
+	mlx->mouse_scale = mlx->mouse_scale * mlx->scale;
+	printf("scale = %f", mlx->scale);
+	ft_calculate_pts(mlx);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, mlx->img.img_ptr, 0, 0);
+
+	return (1); 
+}
+int		ft_mouse_press(int button, int x, int y, t_mlx *mlx)
+{
+	mlx->mouse_xp = mlx->mouse_x;
+	mlx->mouse_yp = mlx->mouse_y;
+	mlx->mouse_x = x * (4.0 ) / WIN_WIDTH * mlx->mouse_scale - mlx->mouse_shift_x;// - ((4.0 * mlx->mouse_scale) / WIN_WIDTH) * WIN_WIDTH / 2;
+	mlx->mouse_y = y * (4.0 ) / WIN_WIDTH *  mlx->mouse_scale - mlx->mouse_shift_y;// - ((4.0 * mlx->mouse_scale) / WIN_WIDTH) * WIN_WIDTH / 2;
+	printf("mouse press : %f, %f\n", mlx->mouse_x, mlx->mouse_y);
 	if (button == 4 || button == 5)
 		ft_zoom(button, mlx);
-		ft_draw(mlx);
+	if ((button == 1 || button == 2) && y > 0)
+		mlx->mouse_b = button;
 	return (1);
+}
+
+int		ft_mouse_release(int button, int x, int y, t_mlx *mlx) //вот тут перемещение отрабатываем на отпускании кнопки
+{
+	double xm;
+	double ym;
+	int i;
+	//printf("заходит по колесу"); //нифига не заходит
+	i = -1;
+	xm = x * (4.0) / WIN_WIDTH *  mlx->mouse_scale - mlx->mouse_shift_x;// - ((4.0 * mlx->mouse_scale) / WIN_WIDTH) * WIN_WIDTH / 2; 
+	ym = y * (4.0) / WIN_WIDTH *  mlx->mouse_scale - mlx->mouse_shift_y;// - ((4.0 * mlx->mouse_scale) / WIN_WIDTH) * WIN_WIDTH / 2;
+	mlx->pnt[mlx->n].re = xm - mlx->mouse_x;  // это при сдвиге
+	mlx->pnt[mlx->n].im = ym - mlx->mouse_y;  // а при масштабировании нужно тоже прописывать
+	mlx->mouse_shift_x += mlx->pnt[mlx->n].re;
+	mlx->mouse_shift_y += mlx->pnt[mlx->n].im;
+	mlx->mouse_b = 0;
+	while (++i < mlx->n)
+	{
+		mlx->pnt[i].re = mlx->pnt[i].re - mlx->pnt[mlx->n].re;
+		mlx->pnt[i].im = mlx->pnt[i].im - mlx->pnt[mlx->n].im;
+	}
+	ft_calculate_pts(mlx);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, mlx->img.img_ptr, 0, 0);
+	return (1);
+}
+
+int ft_mlx_init(t_mlx *mlx)
+{
+	mlx->mlx_ptr = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "fractol");
+	mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
+	mlx->img.data = (int *)mlx_get_data_addr(mlx->img.img_ptr, &(mlx->img.bpp), &(mlx->img.size_l), &(mlx->img.endian));
+return (0);
 }
 
 int main(int argc, char **argv)
@@ -185,22 +288,29 @@ int main(int argc, char **argv)
 		ft_putendl("usage: ./fractol ");
 		return (0);
 	}
-	mlx.pnt = (t_point *)malloc(sizeof(t_point) * WIN_HEIGHT * WIN_WIDTH);
+	mlx.pnt = (t_point *)malloc(sizeof(t_point) * (WIN_HEIGHT * WIN_WIDTH + 1)); // на однушку больше чтобы хранить центр
 	mlx.scale = 1;
+	mlx.mouse_scale = 1;
 	mlx.mouse_x = 0;
 	mlx.mouse_y = 0;
+	mlx.mouse_shift_x = 0;
+	mlx.mouse_shift_y = 0;
 	mlx.mouse_xp = 0;
 	mlx.mouse_yp = 0;
+	ft_mlx_init(&mlx);
 	ft_array_init(&mlx);
-	mlx.mlx_ptr = mlx_init();
+	ft_calculate_pts(&mlx);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+	/* mlx.mlx_ptr = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, argv[1]);
 	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l, &mlx.img.endian);
 	ft_draw(&mlx);
-	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0); */
 	mlx_hook(mlx.win, 17, 1l<<17, &ft_close, 0);
 	mlx_hook(mlx.win, 4, 1L << 2, ft_mouse_press, &mlx);
-	mlx_hook(mlx.win, 6, 1L << 6, ft_mouse_move, &mlx);
-	mlx_hook(mlx.win, 2, 1l<<17, &ft_keypress, &mlx);
+	mlx_hook(mlx.win, 5, 1L << 3, ft_mouse_release, &mlx);
+//	mlx_hook(mlx.win, 6, 1L << 6, ft_mouse_move, &mlx);
+//	mlx_hook(mlx.win, 2, 1l<<17, &ft_keypress, &mlx);
 	mlx_loop(mlx.mlx_ptr);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 12:21:44 by majosue           #+#    #+#             */
-/*   Updated: 2020/01/21 11:38:47 by majosue          ###   ########.fr       */
+/*   Updated: 2020/03/06 08:59:12 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,14 @@
 # include <fcntl.h>
 # include <stdio.h> //for testing
 
-# define WIN_WIDTH 1600
-# define WIN_HEIGHT 1200
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 800
 # define MAX_ITERATIONS 150
 
 typedef union u_colour
 {
 	int colour;
 	unsigned char c[4];
-	//int blue : 8;
-	//int green : 8;
-	//int red : 8;
 } t_colour;
 
 typedef struct	s_img
@@ -44,6 +41,7 @@ typedef struct	s_img
 }				t_img;
 
 typedef struct s_point {
+	int	color;
 	double re;
 	double im;
 } t_point;
@@ -55,12 +53,18 @@ typedef struct	s_mlx
 	int			stcol;
 	int			endcol;
 	double		scale;
+	double		mouse_scale;
+	double		scale_prev;
 	double 		center_x;
 	double		center_y;
 	double		mouse_xp;
 	double		mouse_yp;
 	double		mouse_x;
 	double		mouse_y;
+	double		mouse_shift_x;
+	double		mouse_shift_y;
+	int 		n;
+	int			 mouse_b;
 	t_colour	col;
 	t_point		*pnt;
 	t_img		img;
